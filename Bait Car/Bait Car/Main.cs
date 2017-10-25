36db1b1c07b8740efcb501b9194eba792abff2d1
Scaarus <michael.g.leone@gmail.com>
2017-10-25 16:16:20 -0400

    Added references and set up RPH

---
+++ Bait Car/Bait Car/Main.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+[assembly: Rage.Attributes.Plugin("Bait Car", Description = "Allows user to spawn bait cars and have AI attempt to steal them.", Author = "Scaarus")]
+namespace Bait_Car
+{
+    public class Main
+    {
+        public Main()
+        {
+
+        }
+    }
+}
